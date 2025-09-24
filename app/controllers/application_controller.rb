f7d4109244dcf8286698e220204dc07ac5e415bf
Jeremy Rougerie <j.rougerie@proton.me>
2025-09-24 17:00:33 +0200

    nickname sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # Permettre nickname pour l'inscription
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname ])
+
+    # Permettre nickname pour la modification du compte
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :nickname ])
+  end
 end
--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,13 +1,14 @@
 class CommunityController < ApplicationController
   def index
     @my_community = current_user.community
-    @communities = Community.all.where.not(name: @my_community.name)
-
-    @locations = @communities.map do |c|
-      {
-        community: c,
-        distance: Geocoder::Calculations.distance_between([ @my_community.location.latitude, @my_community.location.longitude ], [  c.location.latitude, c.location.longitude ], units: :km)
-      }
+    unless @my_community.nil?
+      @communities = Community.all.where.not(name: @my_community.name)
+      @locations = @communities.map do |c|
+        {
+          community: c,
+          distance: Geocoder::Calculations.distance_between([ @my_community.location.latitude, @my_community.location.longitude ], [  c.location.latitude, c.location.longitude ], units: :km)
+        }
+      end
     end
   end
 end
